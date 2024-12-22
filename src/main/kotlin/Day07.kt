b541f6307e5b2bfd9ac1a06088664a221440c81c
s1rat-dev <siratsemih@gmail.com>
2024-12-22 13:35:56 +0300

    made cleaner

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -3,35 +3,25 @@ import java.math.BigDecimal
 
 private val path = "src/main/kotlin/inputs/day07.txt"
 private val file = File(path).absoluteFile
+private val calculator = Calculator()
 
-private fun part1() : BigDecimal {
-    val counter = Counter()
+private fun part1(): BigDecimal {
+    val operations = listOf(
+        fun(x: BigDecimal, y: BigDecimal): BigDecimal = x + y,
+        fun(x: BigDecimal, y: BigDecimal): BigDecimal = x * y
+    )
 
-    file.forEachLine {
-        it.split(":").let {
-            val target = BigDecimal(it.first())
-            val nums = it.last().split(" ").mapNotNull { it.toBigDecimalOrNull() }
-
-            counter.calculateP1(list = nums, target = target)
-        }
-    }
-
-    return counter.count
+    return calculator.createAndCalculate(file, operations)
 }
 
-private fun part2() : BigDecimal {
-    val counter = Counter()
+private fun part2(): BigDecimal {
+    val operations = listOf(
+        fun(x: BigDecimal, y: BigDecimal): BigDecimal = x + y,
+        fun(x: BigDecimal, y: BigDecimal): BigDecimal = x * y,
+        fun(x: BigDecimal, y: BigDecimal): BigDecimal = BigDecimal("$x$y")
+    )
 
-    file.forEachLine {
-        it.split(":").let {
-            val target = BigDecimal(it.first())
-            val nums = it.last().split(" ").mapNotNull { it.toBigDecimalOrNull() }
-
-            counter.calculateP2(list = nums, target = target)
-        }
-    }
-
-    return counter.count
+    return calculator.createAndCalculate(file, operations)
 }
 
 fun main() {
@@ -39,10 +29,28 @@ fun main() {
     println(part2())
 }
 
-class Counter {
-    var count: BigDecimal = BigDecimal.ZERO
+class Calculator {
 
-    fun calculateP1(list: List<BigDecimal>, target: BigDecimal) {
+    fun createAndCalculate(file: File, operations: List<(BigDecimal, BigDecimal) -> BigDecimal>): BigDecimal {
+        var count: BigDecimal = BigDecimal.ZERO
+
+        file.forEachLine {
+            it.split(":").let {
+                val target = BigDecimal(it.first())
+                val nums = it.last().split(" ").mapNotNull { it.toBigDecimalOrNull() }
+
+                count += calculate(list = nums, target = target, operations)
+            }
+        }
+
+        return count
+    }
+
+    private fun calculate(
+        list: List<BigDecimal>,
+        target: BigDecimal,
+        operations: List<(BigDecimal, BigDecimal) -> BigDecimal>
+    ): BigDecimal {
         var iterationsCount = 0
 
         fun iterate(current: BigDecimal, list: List<BigDecimal>, target: BigDecimal) {
@@ -51,33 +59,12 @@ class Counter {
                 return
             }
 
-            iterate(current + list.first(), list.drop(1), target)
-            iterate(current * list.first(), list.drop(1), target)
-        }
-
-        iterate(BigDecimal.ZERO, list, target)
-
-        if (iterationsCount != 0) count += target
-    }
-
-    fun calculateP2(list: List<BigDecimal>, target: BigDecimal) {
-        var iterationsCount = 0
-
-        fun iterate(current: BigDecimal, list: List<BigDecimal>, target: BigDecimal) {
-            if (list.isEmpty()) {
-                if (current == target) iterationsCount++
-                return
+            operations.forEach {
+                iterate(it.invoke(current, list.first()), list.drop(1), target)
             }
-
-            iterate(BigDecimal("$current${list.first()}"), list.drop(1), target)
-            iterate(current + list.first(), list.drop(1), target)
-            iterate(current * list.first(), list.drop(1), target)
         }
-
         iterate(BigDecimal.ZERO, list, target)
 
-        if (iterationsCount != 0) count += target
+        return if (iterationsCount != 0) target else BigDecimal.ZERO
     }
-
-
 }
